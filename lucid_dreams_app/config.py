3d22985c91f1f15958be47462c74abc6fcde4327
dimatolpygin <dimatolpygin82@gmail.com>
2025-04-20 20:23:13 +0900

    Update config.py

--- lucid_dreams_app/config.py
+++ lucid_dreams_app/config.py
@@ -2,4 +2,4 @@ import os
 
 # Чтение переменных из окружения (для Railway)
 TELEGRAM_TOKEN = os.getenv("7667201494:AAFJiPBpzaOGc8pKuaPWCjp_RFDLNgBwpkE", "YOUR_BOT_TOKEN_HERE")
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://your-railway-app.up.railway.app/webapp")
+WEBAPP_URL = os.getenv("https://web-app-production-f5c4.up.railway.app/webapp", "https://your-railway-app.up.railway.app/webapp")
